53acf76bbbc82c506764ae18a963be4ba12e9660
Yooshin <yong.yooshin@gmail.com>
2021-05-21 10:16:23 +1000

    ADD serverless plugins installation instructions

---
+++ scripts/installPlugins.sh
@@ -0,0 +1,5 @@
+npm install --save-dev serverless-offline &&
+npm install --save-dev serverless-dynamodb-local &&
+npm install --save webpack &&
+npm install --save serverless-webpack &&
+sls dynamodb install
