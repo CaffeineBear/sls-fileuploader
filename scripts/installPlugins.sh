4dd183534d6def1d8db86795236ea32715fa8367
Yooshin <yong.yooshin@gmail.com>
2021-05-21 10:21:50 +1000

    UDPATE to include aws-adk package on installation script

--- scripts/installPlugins.sh
+++ scripts/installPlugins.sh
@@ -2,4 +2,5 @@ npm install --save-dev serverless-offline &&
 npm install --save-dev serverless-dynamodb-local &&
 npm install --save webpack &&
 npm install --save serverless-webpack &&
+npm install --save aws-sdk && 
 sls dynamodb install
